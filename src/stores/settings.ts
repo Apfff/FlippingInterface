e9da65f049cf5e365ca2c8f82ef1a51d40e9dd37
Apfff <d3monf4il3r@gmail.com>
2025-08-29 05:05:05 +0200

    test build

--- src/stores/settings.ts
+++
@@ -1,7 +0,0 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-
-export const useSettingsStore = defineStore('settings', () => {
-  const showTargetOverlap = ref(false)
-  return { showTargetOverlap }
-})
